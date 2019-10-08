1a8537fe01c1ed592878471855f15e1ab4acf680
Kimmirikwa <kimrodrikwa@gmail.com>
2019-10-08 23:08:37 +0300

    initial results for different datasets

--- titanic/solution.py
+++ titanic/solution.py
@@ -165,36 +165,51 @@ cv_split = ShuffleSplit(n_splits=10, test_size=.3, train_size=.6, random_state=0
 
 # create table to compare MLA metrics
 MLA_columns = ['MLA Name', 'MLA Parameters','MLA Train Accuracy Mean', 'MLA Test Accuracy Mean', 'MLA Test Accuracy 3*STD' ,'MLA Time']
-MLA_compare = pd.DataFrame(columns = MLA_columns)
 
 # create table to compare MLA predictions
 MLA_predict = train_data[Target]
 
-#index through MLA and save performance to table
-row_index = 0
-for alg in MLA:
+training_data = [
+	{
+		"file_name": "scores_without_bins.csv",
+		"data": train_data[train_data_x_calc]
+	},
+	{
+		"file_name": "scores_with_bins.csv",
+		"data": train_data[train_data_x_bin]
+	},
+	{
+		"file_name": "scores_with_dummy.csv",
+		"data": train_data_dummy[train_data_x_dummy]
+	}
+]
 
-    # set name and parameters
-    MLA_name = alg.__class__.__name__
-    MLA_compare.loc[row_index, 'MLA Name'] = MLA_name
-    MLA_compare.loc[row_index, 'MLA Parameters'] = str(alg.get_params())
-    
-    # score model with cross validation: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate
-    cv_results = cross_validate(alg, train_data[train_data_x_bin], train_data[Target], cv=cv_split, return_train_score=True)
+# index through MLA and save performance to table
+for dataset in training_data:
+	MLA_compare = pd.DataFrame(columns = MLA_columns)
+	row_index = 0
+	for alg in MLA:
+	    # set name and parameters
+	    MLA_name = alg.__class__.__name__
+	    MLA_compare.loc[row_index, 'MLA Name'] = MLA_name
+	    MLA_compare.loc[row_index, 'MLA Parameters'] = str(alg.get_params())
+	    
+	    # score model with cross validation: http://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_validate.html#sklearn.model_selection.cross_validate
+	    cv_results = cross_validate(alg, dataset['data'], train_data[Target], cv=cv_split, return_train_score=True)
 
-    MLA_compare.loc[row_index, 'MLA Time'] = cv_results['fit_time'].mean()
-    MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results['train_score'].mean()
-    MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results['test_score'].mean()   
-    # if this is a non-bias random sample, then +/-3 standard deviations (std) from the mean, should statistically capture 99.7% of the subsets
-    MLA_compare.loc[row_index, 'MLA Test Accuracy 3*STD'] = cv_results['test_score'].std()*3   #let's know the worst that can happen!
-    
+	    MLA_compare.loc[row_index, 'MLA Time'] = cv_results['fit_time'].mean()
+	    MLA_compare.loc[row_index, 'MLA Train Accuracy Mean'] = cv_results['train_score'].mean()
+	    MLA_compare.loc[row_index, 'MLA Test Accuracy Mean'] = cv_results['test_score'].mean()   
+	    # if this is a non-bias random sample, then +/-3 standard deviations (std) from the mean, should statistically capture 99.7% of the subsets
+	    MLA_compare.loc[row_index, 'MLA Test Accuracy 3*STD'] = cv_results['test_score'].std()*3   #let's know the worst that can happen!
+	    
 
-    # save MLA predictions - see section 6 for usage
-    alg.fit(train_data[train_data_x_bin], train_data[Target])
-    MLA_predict[MLA_name] = alg.predict(train_data[train_data_x_bin])
-    
-    row_index += 1
+	    # save MLA predictions - see section 6 for usage
+	    alg.fit(train_data[train_data_x_bin], train_data[Target])
+	    MLA_predict[MLA_name] = alg.predict(train_data[train_data_x_bin])
+	    
+	    row_index += 1
 
-MLA_compare.sort_values(by = ['MLA Test Accuracy Mean'], ascending = False, inplace = True)
+	MLA_compare.sort_values(by = ['MLA Test Accuracy Mean'], ascending = False, inplace = True)
 
-MLA_compare[['MLA Name', 'MLA Train Accuracy Mean', 'MLA Test Accuracy Mean']].to_csv('results/scores_with_bins.csv')
+	MLA_compare[['MLA Name', 'MLA Train Accuracy Mean', 'MLA Test Accuracy Mean']].to_csv('results/{}'.format(dataset['file_name']))
